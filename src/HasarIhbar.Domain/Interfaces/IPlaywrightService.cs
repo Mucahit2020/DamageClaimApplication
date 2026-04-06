c7c2e50e8a8f10aac3861692970269456919e09c
Mucahit2020 <mucahitkizilgunes@gmail.com>
2026-04-06 21:04:13 +0300

    son değişiklikler.

---
+++ src/HasarIhbar.Domain/Interfaces/IPlaywrightService.cs
@@ -0,0 +1,11 @@
+namespace HasarIhbar.Domain.Interfaces
+{
+    public interface IPlaywrightService : IAsyncDisposable
+    {
+        Task InitializeAsync(bool headless = false);
+        Task<bool> NavigateAsync(string url);
+        Task<bool> SelectTabAsync(int tabIndex);
+        Task TakeScreenshotAsync(string path);
+        Task CloseAsync();
+    }
+}
